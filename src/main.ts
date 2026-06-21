f0249da251a2e041ed18fe93d6a661b538975d8b
Sudam Ranasinghe <sudamranasinghe98@gmai.com>
2026-06-21 17:11:07 +0530

    added the production port

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      process.env.FRONTEND_URL || '',
+    ],
     credentials: true,
   });
 
-  await app.listen(3001);
-  console.log('API running on http://localhost:3001');
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port);
+
+  console.log(`API running on port ${port}`);
 }
+
 bootstrap();
